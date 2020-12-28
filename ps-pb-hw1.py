7a1bed45f7c3abf92bd00c2c8c3ec5da0464fd82
Mark Santos <marksantos@MacBook-Pro-Mark.local>
2020-12-28 18:51:39 +0300

    правильная версия

--- ps-pb-hw1.py
+++ ps-pb-hw1.py
@@ -68,12 +68,41 @@ $$$$$$$$
                            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
                            $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 """
-i=0
-while i<4 :
-    print(sl1, end = '\r')
-    time.sleep(1)
-    os.system('clear')
-    print(sl2)
-    time.sleep(1)
-    os.system('clear')
-    i +=1
+
+print(sl1, end = '\r')
+time.sleep(1)
+os.system('clear')
+print(sl2)
+time.sleep(1)
+os.system('clear')
+
+print(sl1, end = '\r')
+time.sleep(1)
+os.system('clear')
+print(sl2)
+time.sleep(1)
+os.system('clear')
+
+print(sl1, end = '\r')
+time.sleep(1)
+os.system('clear')
+print(sl2)
+time.sleep(1)
+os.system('clear')
+
+print(sl1, end = '\r')
+time.sleep(1)
+os.system('clear')
+print(sl2)
+time.sleep(1)
+os.system('clear')
+
+#i=0
+#while i<4 :
+#    print(sl1, end = '\r')
+#    time.sleep(1)
+#    os.system('clear')
+#    print(sl2)
+#    time.sleep(1)
+#    os.system('clear')
+#    i +=1
